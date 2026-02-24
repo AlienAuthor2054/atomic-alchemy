4a328d0ce28231717744708477bf3d49e5a4a31f
AlienAuthor2054 <alienauthor2054@proton.me>
2026-02-24 16:54:40 +0800

    Update Point class
    - Convert from NamedTuple to dataclass while preserving tuple unpacking.
    - Add length and unit vector helper methods.

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
+from __future__ import annotations
 import tkinter as tk
 from tkinter.font import Font as TkFont
 from random import randrange
 from typing import NamedTuple
+from dataclasses import dataclass, astuple
 
 WINDOW_X = 960
 WINDOW_Y = 540
 
-class Point(NamedTuple):
-    x: int
-    y: int
+@dataclass
+class Point():
+    x: float
+    y: float
 
-    def __add__(self, other):
+    def __add__(self, other: Point):
         return Point(self.x + other.x, self.y + other.y)
     
-    def __sub__(self, other):
+    def __sub__(self, other: Point):
         return Point(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
+    def __mul__(self, other: float):
         return Point(self.x * other, self.y * other)
+    
+    def __truediv__(self, other: float):
+        return Point(self.x / other, self.y / other)
+    
+    def __len__(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def __iter__(self):
+        # Return an iterator from astuple to enable tuple unpacking
+        return iter(astuple(self))
+
+    def unit(self, mul: float = 1.0):
+        return self / len(self) * mul
 
 # TODO: Use Canvas objects instead of widgets
 class Atom():
