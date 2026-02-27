09b2480a7c7e14ad78a58206d71ec39841fd63aa
AlienAuthor2054 <alienauthor2054@proton.me>
2026-02-27 19:57:41 +0800

    Add Lab draggable

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from time import perf_counter
 from dataclasses import dataclass, astuple
 from random import randrange
 from abc import ABC
+import math
 
 WINDOW_X = 960
 WINDOW_Y = 540
@@ -37,6 +38,9 @@ class Point():
 
     def unit(self, mul: float = 1.0):
         return self / len(self) * mul
+    
+    def is_zero(self):
+        return math.isclose(self.x, 0) and math.isclose(self.y, 0)
 
 class Draggable(ABC):
     def __init__(self, game: Game, tag: str, pos: Point, vel: Point = Point(0, 0), limit_to_window: bool = False):
@@ -63,15 +67,19 @@ class Draggable(ABC):
     def on_release(self, event):
         self.dragging = False
     
-    def on_drag(self, event):
+    def on_drag(self, event) -> Point:
         if not self.dragging:
-            return
+            return Point(0, 0)
         mouse_pos = Point(event.x, event.y)
-        offset = mouse_pos - self.last_mouse_pos
-        self.drag_pos += offset
+        mouse_offset = mouse_pos - self.last_mouse_pos
+        self.drag_pos += mouse_offset
         self.last_mouse_pos = mouse_pos
-        if not self.limit_to_window or self.is_in_window(self.drag_pos - self.pos):
+        offset = self.drag_pos - self.pos
+        if not self.limit_to_window or self.is_on_window(offset, 50):
             self.move_to(self.drag_pos)
+            return offset
+        else:
+            return Point(0, 0)
     
     def move_to(self, pos):
         self.pos = pos
@@ -79,14 +87,17 @@ class Draggable(ABC):
             self.tag, pos.x, pos.y
         )
         self.canvas.update_idletasks()
+    
+    def move(self, offset: Point):
+        self.move_to(self.pos + offset)
 
-    def is_in_window(self, offset: Point = Point(0, 0)):
+    def is_on_window(self, offset: Point = Point(0, 0), padding: int = 0):
         x1, y1, x2, y2 = self.canvas.bbox(self.tag)
-        x1 += offset.x
-        x2 += offset.x
-        y1 += offset.y
-        y2 += offset.y
-        return x1 > 0 and y1 > 0 and x2 < WINDOW_X and y2 < WINDOW_Y
+        x1 += offset.x + padding
+        x2 += offset.x - padding
+        y1 += offset.y + padding
+        y2 += offset.y - padding
+        return x1 < WINDOW_X and y1 < WINDOW_Y and x2 > 0 and y2 > 0
 
     def physics_process(self, delta):
         if self.dragging:
@@ -108,17 +119,18 @@ class Atom(Draggable):
         self.id = Atom.next_id
         Atom.next_id += 1
         tag = "atom" + str(self.id)
-        radius = 36
+        radius = 30
         pad = radius
         center = Point(-pad, randrange(pad, WINDOW_Y - pad))
         pos = Point(center.x - radius, center.y - radius)
         vel = Point(randrange(75, 200), 0)
         super().__init__(game, tag, pos, vel)
+        self.in_lab = False
         self.canvas.create_rectangle(
             center.x - radius, center.y - radius,
             center.x + radius, center.y + radius,
             fill="black",
-            tags=tag,
+            tags=(tag, "atom"),
         )
         label_font = TkFont(size=radius)
         self.canvas.create_text(
@@ -127,9 +139,54 @@ class Atom(Draggable):
             text="H",
             font=label_font,
             fill="gray",
-            tags=tag,
+            tags=(tag, "atom"),
             state=tk.DISABLED,
         )
+        self.canvas.tag_lower(tag, "lab")
+    
+    def on_click(self, event):
+        super().on_click(event)
+        self.canvas.tag_raise(self.tag)
+    
+    def on_release(self, event):
+        super().on_release(event)
+        x1, y1, x2, y2 = self.canvas.bbox("lab")
+        if x1 < event.x < x2 and y1 < event.y < y2:
+            if not self.in_lab:
+                self.in_lab = True
+                self.game.lab.contents.add(self)
+                self.game.physics_objects.remove(self)
+                self.canvas.addtag_withtag("lab_obj", self.tag)
+                self.vel = Point(0, 0)
+            self.canvas.tag_raise(self.tag, "lab")
+        else:
+            if self.in_lab:
+                self.in_lab = False
+                self.game.lab.contents.remove(self)
+                self.game.physics_objects.add(self)
+                self.canvas.dtag(self.tag, "lab_obj")
+                self.vel = Point(100, 0)
+            self.canvas.tag_lower(self.tag, "lab")
+
+class Lab(Draggable):
+    def __init__(self, game: Game):
+        tag = "lab"
+        super().__init__(game, tag, Point(0, 0), Point(0, 0), True)
+        self.contents: set[Draggable] = set()
+        self.canvas.create_rectangle(
+            0, 0,
+            600, 300,
+            fill="gray",
+            tags=tag,
+        )
+    
+    def on_drag(self, event):
+        offset = super().on_drag(event)
+        if offset.is_zero():
+            return offset
+        for obj in self.contents:
+            obj.move(offset)
+        return offset
 
 class Game():
     def __init__(self, root: tk.Tk) -> None:
@@ -139,6 +196,7 @@ class Game():
         canvas = tk.Canvas(root, width=WINDOW_X, height=WINDOW_Y)
         canvas.pack()
         self.canvas = canvas
+        self.lab = Lab(self)
 
     def start(self):
         self.atom_spawn_loop()
