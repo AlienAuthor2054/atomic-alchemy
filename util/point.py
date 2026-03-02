29ccd2d7d10d6661f6d9c4cdc12c6d3ff08d45d1
AlienAuthor2054 <alienauthor2054@proton.me>
2026-03-02 12:39:50 +0800

    Make Point length method work for floats
    Replaced method header with Point.length(), len(Point) could only return int.

--- util/point.py
+++ util/point.py
@@ -20,15 +20,15 @@ class Point():
     def __truediv__(self, other: float):
         return Point(self.x / other, self.y / other)
     
-    def __len__(self):
+    def length(self) -> float:
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def __iter__(self):
         # Return an iterator from astuple to enable tuple unpacking
         return iter(astuple(self))
 
-    def unit(self, mul: float = 1.0):
-        return self / len(self) * mul
+    def unit(self, mul: float = 1.0) -> Point:
+        return self / self.length() * mul
     
     def is_zero(self):
         return math.isclose(self.x, 0) and math.isclose(self.y, 0)
