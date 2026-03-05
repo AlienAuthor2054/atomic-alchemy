9bf51fc74b05d7da3cde7542b051694abc781f16
AlienAuthor2054 <alienauthor2054@proton.me>
2026-03-06 00:33:14 +0800

    Add double and triple bonds
    Left-click an existing bond to upgrade it. Bonds have also been made wider to be easier to click.

--- core/atom.py
+++ core/atom.py
@@ -282,6 +282,13 @@ class Atom(Draggable):
 
 class Bond():
     LENGTH = BOND_LENGTH
+    LINE_SEPERATION = 20
+    LINE_OFFSETS = [
+        [0.0],
+        [-0.5, 0.5], 
+        [-1.0, 0.0, 1.0],
+    ]
+    LINE_WIDTH = 15
     next_id = 1
 
     def __init__(self, atom1: Atom, atom2: Atom, bond_order: int) -> None:
@@ -293,16 +300,28 @@ class Bond():
         self.canvas = canvas
         self.atom1 = atom1
         self.atom2 = atom2
-        self.order = bond_order
-        self.canvas.create_line(
-            *atom1.center, *atom2.center, 
-            fill="black",
-            width=10,
-            tags=(self.tag, "bond"),
-        )
-        self.update_layering()
+        self._order = bond_order
+        self.lines: list[int | None] = [None, None, None]
+        self.update_lines()
+        self.update_display()
+        canvas.tag_bind(tag, "<ButtonPress-1>", self.on_left_click)
         canvas.tag_bind(tag, "<ButtonPress-3>", self.on_right_click)
     
+    @property
+    def order(self) -> int:
+        return self._order
+
+    @order.setter
+    def order(self, new: int) -> None:
+        self._order = new
+        self.update_lines()
+
+    def on_left_click(self, event) -> None:
+        try:
+            self.atom1.add_bond(self.atom2, 1)
+        except ValueError as e:
+            print(e)
+
     def on_right_click(self, event) -> None:
         self.atom1.remove_bond(self.atom2, 1)
 
@@ -310,8 +329,38 @@ class Bond():
         self.canvas.tag_lower(self.tag, "atom")
         self.canvas.tag_raise(self.tag, "lab")
 
+    def update_lines(self) -> None:
+        for i in range(len(self.lines)):
+            line_id = self.lines[i]
+            if i >= self.order:
+                if line_id is not None:
+                    self.canvas.itemconfigure(line_id, state="hidden")
+                break
+            if line_id is None:
+                line_id = self.canvas.create_line(
+                    *self.atom1.center, *self.atom2.center, 
+                    fill="black",
+                    width=Bond.LINE_WIDTH,
+                    tags=(self.tag, "bond"),
+                )
+                self.lines[i] = line_id
+            self.canvas.itemconfigure(line_id, state="normal")
+        self.update_display()
+
     def update_display(self) -> None:
-        self.canvas.coords(self.tag, *self.atom1.center, *self.atom2.center)
+        atom_diff = self.atom2.center - self.atom1.center
+        if atom_diff.is_zero():
+            atom_diff = Point(1, 0)
+        offset_unit = Point(atom_diff.y, atom_diff.x).unit(Bond.LINE_SEPERATION)
+        line_offsets = Bond.LINE_OFFSETS[self.order - 1]
+        for i in range(self.order):
+            line_id = self.lines[i]
+            if line_id is None:
+                break
+            offset = offset_unit * line_offsets[i]
+            self.canvas.coords(line_id,
+                *(self.atom1.center + offset), *(self.atom2.center + offset)
+            )
         self.update_layering()
     
     def remove(self) -> None:
