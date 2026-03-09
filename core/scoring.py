2e88184c1d47de44f7cf9b3836dcbb71f1e90ad8
AlienAuthor2054 <alienauthor2054@proton.me>
2026-03-09 10:10:14 +0800

    Add convenient bond scoring
    Allow scoring the bond itself, not just a change in bond order.

--- core/scoring.py
+++ core/scoring.py
@@ -5,12 +5,20 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .bond import Bond
 
-def score_bond_change(bond: Bond, prev_order: int, new_order: int) -> int:
-    symbol_list = [bond.atom1.element.symbol, bond.atom2.element.symbol]
+def _get_score_list(symbol_list: list[str]) -> list[int]:
     score_list = BOND_SCORES.get(''.join(symbol_list))
     if score_list is None:
         symbol_list.reverse()
         score_list = BOND_SCORES[''.join(symbol_list)]
+    return score_list
+
+def score_bond(bond: Bond) -> int:
+    symbol_list = [bond.atom1.element.symbol, bond.atom2.element.symbol]
+    return _get_score_list(symbol_list)[bond.order-1]
+
+def score_bond_change(bond: Bond, prev_order: int, new_order: int) -> int:
+    symbol_list = [bond.atom1.element.symbol, bond.atom2.element.symbol]
+    score_list = _get_score_list(symbol_list)
     prev_energy = score_list[prev_order-1] if prev_order > 0 else 0
     new_energy = score_list[new_order-1] if new_order > 0 else 0
     return new_energy - prev_energy
